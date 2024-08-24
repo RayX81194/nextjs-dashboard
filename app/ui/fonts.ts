419f18bd6b8740356249abf4c083eeea6b73f156
rayx81194 <mani.phartiyal@gmail.com>
2024-08-24 13:40:52 +0530

    new

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,8 @@
-import { Manrope, Inter, } from 'next/font/google';
+import { Manrope, Inter,Lusitana } from 'next/font/google';
 
 export const manrope = Manrope({ subsets: ['latin'] });
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
